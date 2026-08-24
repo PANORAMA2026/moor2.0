752392024d3f6282f2bdb6c1022d2efeea65885f
PANORAMA2026 <pobridge@carnival.com>
2026-08-24 15:52:56 -0700

    Create mooring_physics.py

---
+++ core/mooring_physics.py
@@ -0,0 +1,8 @@
+"""
+core/mooring_physics.py
+Interfaccia unica per i moduli di calcolo navale e ormeggio.
+"""
+
+from .fatigue_model import *
+from .hydrodynamic_forces import *
+from .line_mechanics import *
