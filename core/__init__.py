43224738cb68a9974dc814d637f0c2dfdbee9052
PANORAMA2026 <pobridge@carnival.com>
2026-08-24 15:48:42 -0700

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,1 +1,12 @@
+"""
+core/__init__.py
+Esporta le funzioni principali dei singoli sottomoduli.
+"""
 
+from .fatigue_model import *
+from .hydrodynamic_forces import calculate_environmental_forces
+from .line_mechanics import (
+    calculate_line_geometry,
+    calculate_wind_operability_envelope,
+    solve_line_tensions_3d,
+)
