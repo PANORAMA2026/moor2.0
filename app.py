20560570aa041d9ed12d3c1d536cb7107688db38
PANORAMA2026 <pobridge@carnival.com>
2026-08-24 22:50:47 -0700

    Update app.py

--- app.py
+++ app.py
@@ -17,9 +17,9 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 # Import dei moduli configurazione e database
 from config.constants import (
-    DEFAULT_BOLLARDS,
     DEFAULT_SHIP,
-    PORT_COORDINATES,
+    OFFSET_PLATFORM_AFT_M,
+    OFFSET_PLATFORM_FWD_M,
 )
 
 # Import DIRETTO dai singoli file della cartella core
