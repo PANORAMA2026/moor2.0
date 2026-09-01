32ec5e4df31086bfaf2e581080dfef6e459ae3f8
PANORAMA2026 <pobridge@carnival.com>
2026-09-01 19:18:05 +0200

    chore: establish engineering foundation

---
+++ domain/__init__.py
@@ -0,0 +1,5 @@
+"""Canonical engineering domain models for OpenMooring.
+
+This package is intentionally independent from Streamlit and persistence code.
+Migration from legacy DataFrame-based structures will be incremental.
+"""
